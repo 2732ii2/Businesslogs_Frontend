25a104343149296585ad21cac95f27533683ab0f
2732ii2 <mo.anas123khan@gmail.com>
2024-04-06 13:02:46 +0530

    daily updates

--- src/Components/Home/Homepage.js
+++ src/Components/Home/Homepage.js
@@ -15,14 +15,16 @@ export default function Homepage() {
     setuser(JSON.parse(localStorage.getItem("userdata")))
   },[])
   const eventCall=()=>{
+    alert("we will send you to that page");
     if(update===1){
-      navi("/viewdata");
+      // navi("/viewdata");
+      // alert
     }
     else if(update===2){
-      navi("/dataInsert");
+      // navi("/dataInsert");
     }
     else if(update===3){
-      navi("/expensetracker");
+      // navi("/expensetracker");
     }
   }
   var list_=["View Logs"," Insert Logs","Expenditure"];
@@ -42,11 +44,18 @@ export default function Homepage() {
       <div className='select_'> Select any one</div>
       {
         list_.map((e,i)=>{
-          return <div className='options_'  key={i} onClick={()=>setupdate(i+1)}>  <div className='dot' style={{background:`${update-1}`==`${i}`?"rgb(131, 244, 131)":null}}> </div> {e}</div>
+          return <div className='options_'  key={i} onClick={()=>setupdate(i+1)} style={{border:`${update-1}`==`${i}`?"2px dashed black":null,boxShadow:`${update-1}`==`${i}`?"none":null}}>  <div className='dot' style={{background:`${update-1}`==`${i}`?"rgb(131, 244, 131)":null}}> </div> {e}</div>
         })
       }
       <button className='homebutton' onClick={eventCall} disabled={!update}>Submit</button>
 
+      { !user_?
+      <div className='SorryDiv'>
+          <h1> ! Sorry , 404 error </h1>
+          <p>Go back <span onClick={()=>navi("/")}>Login</span> </p>
+      </div> 
+      :null
+      } 
     </div>
   )
 }
